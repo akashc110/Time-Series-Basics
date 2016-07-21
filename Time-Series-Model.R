339cae78c8afa27b44d2cc1736cf2f8e2ed54ad8
Akash Chitnis <Akash Chitnis>
2016-07-21 08:21:39 -0400

    #COmmit2

--- Time-Series-Model.R
+++ Time-Series-Model.R
@@ -1,1 +1,17 @@
 #Time Series Analysis
+require(astsa)
+require(TTR)
+
+#Plotting the Dow Jones Industrial Average
+
+djia <- getYahooData("^DJI", start = 20060420, end = 20160420, freq = "daily")
+djiar <- diff(log(djia$Close))[-1]
+plot(djiar,main = "DJIA Returns", type = "n")
+lines(djiar)
+
+head(gtemp)
+
+
+plot(jj, type = "o", ylab = "Quaterly Earnings Per Share")
+plot(gtemp, type = "o", ylab = "Global Temperature Deviations")
+plot(log(jj))
